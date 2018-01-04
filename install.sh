a02b8142ff8e89b7fdeab7deca441af3bcedb742
Bharath Upadhya <1992bharath@gmail.com>
2018-01-04 20:08:02 +0530

    fix bugs

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 # install python prereqs
-add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
 
@@ -13,10 +12,7 @@ python -m pip install pip==8.1.2
 pip install -U pip setuptools
 
 # Installing virtualenv-15.1.0
-wget -nv https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz#md5=44e19f4134906fe2d75124427dc9b716
-tar xvfz virtualenv-15.1.0.tar.gz
-cd virtualenv-15.1.0
-python setup.py install
+pip install virtualenv==15.1.0
 cd /
 #!/bin/bash
 for file in /u16pyt/version/*;
